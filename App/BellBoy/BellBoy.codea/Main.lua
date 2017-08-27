7f4c8b88a4f5ead25c41d8bddbf36e5c3df81cb3
Austin Lubetkin <bocaaust@gmail.com>
2017-08-27 02:42:40 -0400

    Finish App

--- App/BellBoy/BellBoy.codea/Main.lua
+++ App/BellBoy/BellBoy.codea/Main.lua
@@ -16,8 +16,9 @@ function setup()
     boyTimer = 0
     xw = WIDTH/1024
     textWhite = color(250,244,244,255)
+    textGrey = color(149,152,154,255)
     string = ""
-    screenList = {"Project:room","Project:name","Project:home","Project:roomservice","Project:roomservice","Project:roomservice","Project:roomserviceerror"}
+    screenList = {"Project:room","Project:name","Project:home","Project:roomservice","Project:roomservice","Project:roomservice","Project:roomserviceerror","Project:luggage","Project:luggage","Project:luggageerror","Project:towels","Project:towels","Project:towelserror","Project:ice","Project:ice","Project:iceerror"}
     faceList = {"Project:normal","Project:blink","Project:wink"}
 end
 
@@ -33,6 +34,13 @@ function keyboard(key)
         hideKeyboard()
     end
     end
+end
+
+function complete(phrase)
+    font("DS-Digital")
+    fontSize(100)
+    fill(textGrey)
+    text(phrase,WIDTH/2, HEIGHT-597*xw)
 end
 
 -- This function gets called once every frame
@@ -68,17 +76,46 @@ end
         fill(255,255,255,.89*255)
         rect(WIDTH/2,HEIGHT-597,WIDTH,194*xw)
 
-        if delivered > 0 then
+        if math.floor(delivered) > 0 then
             sprite("Project:cancel",717*xw,HEIGHT-596*xw,477*xw)
             delivered = delivered - 1./FPS
             fill(87,87,87,255)
             x = 141*xw
             y = 172*xw
             for i=0,math.floor(100*delivered/initial) do
-                ellipse(x+72.5*xw*math.cos(i*2*math.pi/100),y+72.5*xw*math.sin(i*2*math.pi/100),16)
+                ellipse(x+72.5*xw*math.cos((i)*2*math.pi/100+math.pi/2),y+72.5*xw*math.sin(i*2*math.pi/100+math.pi/2),16)
+            end
+            if math.floor(delivered) == 0 then
+                delivered = 0
+                screen = screen + 1
+            end
+            fill(textGrey)
+            fontSize(120*xw)
+            font("DS-Digital")
+            if math.floor(delivered%60) > 9 then
+                text((math.floor(delivered/60))..":"..math.floor(delivered%60),350*xw,HEIGHT-596*xw)
+            else
+                text((math.floor(delivered/60))..":0"..math.floor(delivered%60),350*xw,HEIGHT-596*xw)
+            end
+            if CurrentTouch.state == BEGAN and touched and CurrentTouch.x > 683*xw and CurrentTouch.y < HEIGHT*3/8 then
+                touched = false
+                delivered = 0
+                screen = 3
             end
         end
 
+        if screen == 6 then
+           complete("ROOM SERVICE DELIVERED")
+        end
+        if screen == 9 then
+            complete("LUGGAGE DELIVERED")
+        end
+        if screen == 12 then
+            complete("TOWELS DELIVERED")
+        end
+        if screen == 15 then
+            complete("ICE DELIVERED")
+        end
         fill(0,0,0,181)
         rect(201.5*xw,HEIGHT-192*xw,290*xw,271*xw)
         font("DS-Digital")
@@ -92,22 +129,28 @@ end
             touched = false
             if delivered > 0 then
                 screen = screen + 2
-            elseif CurrentTouch.y < HEIGHT-219*xw and CurrentTouch.y > HEIGHT-(219+25)*xw then
-                screen = 4
+            end
+            for i=1,4 do
+                if CurrentTouch.y < HEIGHT-(201-25)*xw-(50*(i-1))*xw and CurrentTouch.y > HEIGHT-(201+25)*xw-(50*(i-1))*xw then
+                    screen = 4+(i-1)*3
+                    if i > 1 then screen = screen + 1 end
+                end
             end
         end
         text("Make an order:",159*xw,HEIGHT-167*xw)
-        text("Towels",119*xw,HEIGHT-xw*201)
-        text("Room Service",154*xw,HEIGHT-xw*232)
-        text("Ice",100*xw,HEIGHT-xw*263)
-        text("Luggage",128*xw,HEIGHT-xw*294)
+        text("Towels",119*xw,HEIGHT-xw*263)
+        text("Room Service",154*xw,HEIGHT-xw*201)
+        text("Ice",100*xw,HEIGHT-xw*294)
+        text("Luggage",128*xw,HEIGHT-xw*232)
         elseif CurrentTouch.state == BEGAN and touched and screen ~= 4 then
             touched = false
             screen = screen - 2
+        elseif delivered > 0 then
+            delivered = delivered - 1./FPS
         end
         if screen == 3 then
             text(username.." - "..room,WIDTH*3.4/5,HEIGHT*7/8)
-            boyTimer = boyTimer + 1./FPS.
+            boyTimer = boyTimer + 1./FPS
             if boyTimer <= 5 or (boyTimer <= 10.2 and boyTimer >= 5.2) or (boyTimer >= 10.4 and boyTimer <= 15.4) then
                 face = 1
             elseif (boyTimer > 5 and boyTimer < 5.2) or (boyTimer > 10 and boyTimer < 10.2) then
@@ -118,7 +161,7 @@ end
                 boyTimer = 0
             end
             sprite(faceList[face],699*xw,HEIGHT-264*xw,411*xw,300*xw)
-            fill(149,152,154,255)
+            fill(textGrey)
             fontSize(70)
             font("DS-Digital")
             text("WELCOME, HOW CAN BELLBOY",WIDTH/2,HEIGHT-555*xw)
@@ -142,7 +185,7 @@ end
                 if CurrentTouch.y < HEIGHT-575*xw then
                     touched = false
                     screen = screen + 1
-                    delivered = 330
+                    delivered = math.random(300,900)
                     initial = delivered
                 end
                 if CurrentTouch.y > HEIGHT/4*3 and CurrentTouch.x > WIDTH*6.5/8 then
